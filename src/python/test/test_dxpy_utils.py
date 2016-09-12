solvebio/dx-toolkit

commit 384e8ef40fbc298f87b103960d87e3e834d2d915

PTFM-13671 dx download improvements
Summary:
- dxpy.DXHTTPRequest uses urllib3 directly instead of via requests. This is faster in many cases.

- dxpy.bindings.dxfile_functions.download_dxfile uses a plain concurrent.futures.ThreadPoolExecutor instead of our custom version, which is much slower.

- download_dxfile downloads along part boundaries.

- download_dxfile retrieves part metadata from file description, validates any data already downloaded, resumes on the next incomplete part, and validates each downloaded part against its MD5 checksum.

- download_dxfile avoids using DXFile read buffers, eliminating unneded buffer copies which were observed to be slow as well.

- download_dxfile avoids setting the Range header for single-part files, allowing nginx to enable gzip compression.

Test Plan: dxpy integration tests

Reviewers: eworley, psung

Reviewed By: psung

Subscribers: psung

Differential Revision: https://dnanexus.phacility.com/D513

src/python/dxpy/bindings/dxfile_functions.py
@@ -22,12 +22,21 @@ The following helper functions are useful shortcuts for interacting with File ob
 
 '''
 
-from __future__ import (print_function, unicode_literals)
+from __future__ import print_function, unicode_literals, division, absolute_import
 
 import os, sys, math, mmap, stat
+import hashlib
+import traceback
+from collections import defaultdict
+from multiprocessing import cpu_count
+from concurrent.futures import ThreadPoolExecutor
 
 import dxpy
+from .. import logger, DXHTTPRequest
 from . import dxfile, DXFile
+from .dxfile import FILE_REQUEST_TIMEOUT
+from ..compat import open
+from ..exceptions import DXFileError, DXPartLengthMismatchError, DXChecksumMismatchError
 
 def open_dxfile(dxid, project=None, read_buffer_size=dxfile.DEFAULT_BUFFER_SIZE):
     '''
@@ -79,18 +88,25 @@ def new_dxfile(mode=None, write_buffer_size=dxfile.DEFAULT_BUFFER_SIZE, **kwargs
     dx_file.new(**kwargs)
     return dx_file
 
-def download_dxfile(dxid, filename, chunksize=dxfile.DEFAULT_BUFFER_SIZE, append=False, show_progress=False,
+_download_retry_counter = defaultdict(lambda: 3)
+_executor = None
+def _get_executor():
+    global _executor
+    if _executor is None:
+        _executor = ThreadPoolExecutor(max_workers=cpu_count())
+    return _executor
+
+def download_dxfile(dxfile_or_id, filename, chunksize=dxfile.DEFAULT_BUFFER_SIZE, append=False, show_progress=False,
                     project=None, **kwargs):
     '''
-    :param dxid: Remote file ID
-    :type dxid: string
+    :param dxfile_or_id: Remote file handler or ID
+    :type dxfile_or_id: DXFile or string
     :param filename: Local filename
     :type filename: string
     :param append: If True, appends to the local file (default is to truncate local file if it exists)
     :type append: boolean
 
-    Downloads the remote file with object ID *dxid* and saves it to
-    *filename*.
+    Downloads the remote file referenced by *dxfile_or_id* and saves it to *filename*.
 
     Example::
 
@@ -98,7 +114,7 @@ def download_dxfile(dxid, filename, chunksize=dxfile.DEFAULT_BUFFER_SIZE, append
 
     '''
 
-    def print_progress(bytes_downloaded, file_size):
+    def print_progress(bytes_downloaded, file_size, action="Downloaded"):
         num_ticks = 60
 
         effective_file_size = file_size or 1
@@ -108,39 +124,145 @@ def download_dxfile(dxid, filename, chunksize=dxfile.DEFAULT_BUFFER_SIZE, append
         ticks = int(round((bytes_downloaded / float(effective_file_size)) * num_ticks))
         percent = int(round((bytes_downloaded / float(effective_file_size)) * 100))
 
-        fmt = "[{done}{pending}] Downloaded {done_bytes:,}{remaining} bytes ({percent}%) {name}"
-        sys.stderr.write(fmt.format(done=('=' * (ticks - 1) + '>') if ticks > 0 else '',
-                                    pending=' ' * (num_ticks - ticks),
+        fmt = "[{done}{pending}] {action} {done_bytes:,}{remaining} bytes ({percent}%) {name}"
+        sys.stderr.write(fmt.format(action=action,
+                                    done=("=" * (ticks - 1) + ">") if ticks > 0 else "",
+                                    pending=" " * (num_ticks - ticks),
                                     done_bytes=bytes_downloaded,
-                                    remaining=' of {size:,}'.format(size=file_size) if file_size else "",
+                                    remaining=" of {size:,}".format(size=file_size) if file_size else "",
                                     percent=percent,
                                     name=filename))
         sys.stderr.flush()
         sys.stderr.write("\r")
         sys.stderr.flush()
 
-    file_size = None
     _bytes = 0
 
-    mode = 'ab' if append else 'wb'
-    with DXFile(dxid, mode='r', project=project, read_buffer_size=chunksize) as dxfile, open(filename, mode) as fd:
-        if show_progress:
-            print_progress(0, None)
-        while True:
-            file_content = dxfile.read(chunksize, **kwargs)
-            if file_size is None:
-                file_size = dxfile._file_length
+    if isinstance(dxfile_or_id, DXFile):
+        dxfile = dxfile_or_id
+    else:
+        dxfile = DXFile(dxfile_or_id, mode="r", project=project)
 
+    dxfile_desc = dxfile.describe(fields={"parts"}, default_fields=True, **kwargs)
+    parts = dxfile_desc["parts"]
+    parts_to_get = sorted(parts, key=int)
+    file_size = dxfile_desc.get("size")
+    chunks_to_get = []
+
+    # Warm up the download URL cache in the file handler, to avoid all worker threads trying to fetch it simultaneously
+    dxfile.get_download_url(**kwargs)
+
+    offset = 0
+    for part_id in parts_to_get:
+        parts[part_id]["start"] = offset
+        offset += parts[part_id]["size"]
+
+    def get_chunk(chunk_info):
+        url, headers = dxfile.get_download_url(**kwargs)
+        # If we're fetching the whole object in one shot, avoid setting the Range header to take advantage of gzip
+        # transfer compression
+        if len(parts) > 1 or len(chunks_to_get) > 1:
+            headers["Range"] = "bytes={}-{}".format(chunk_info["start"], chunk_info["end"])
+        chunk_info["data"] = DXHTTPRequest(url, b"", method="GET", headers=headers, auth=None, jsonify_data=False,
+                                           prepend_srv=False, always_retry=True, timeout=FILE_REQUEST_TIMEOUT,
+                                           decode_response_body=False)
+        return chunk_info
+
+    if append:
+        fh = open(filename, "ab")
+    else:
+        try:
+            fh = open(filename, "rb+")
+        except IOError:
+            fh = open(filename, "wb")
+
+    if show_progress:
+        print_progress(0, None)
+
+    if fh.mode == "rb+":
+        last_verified_part, last_verified_pos, max_verify_chunk_size = None, 0, 1024*1024
+        try:
+            for part_id in parts_to_get:
+                part_info = parts[part_id]
+                bytes_to_read = part_info["size"]
+                hasher = hashlib.md5()
+                while bytes_to_read > 0:
+                    chunk = fh.read(min(max_verify_chunk_size, bytes_to_read))
+                    if len(chunk) < min(max_verify_chunk_size, bytes_to_read):
+                        raise DXFileError("Local data for part {} is truncated".format(part_id))
+                    hasher.update(chunk)
+                    bytes_to_read -= max_verify_chunk_size
+                if hasher.hexdigest() != part_info["md5"]:
+                    raise DXFileError("Checksum mismatch when verifying downloaded part {}".format(part_id))
+                else:
+                    last_verified_part = part_id
+                    last_verified_pos = fh.tell()
+                    if show_progress:
+                        _bytes += part_info["size"]
+                        print_progress(_bytes, file_size, action="Verified")
+        except (IOError, DXFileError) as e:
+            logger.debug(e)
+        fh.seek(last_verified_pos)
+        fh.truncate()
+        if last_verified_part is not None:
+            del parts_to_get[:parts_to_get.index(last_verified_part)+1]
+        if show_progress and len(parts_to_get) < len(parts):
+            print_progress(last_verified_pos, file_size, action="Resuming at")
+        logger.debug("Verified %s/%d downloaded parts", last_verified_part, len(parts_to_get))
+
+    for part_id in parts_to_get:
+        part_info = parts[part_id]
+        for chunk_start in range(part_info["start"], part_info["start"] + part_info["size"], chunksize):
+            chunk_end = min(chunk_start + chunksize, part_info["start"] + part_info["size"]) - 1
+            chunks_to_get.append(dict(part=part_id, start=chunk_start, end=chunk_end))
+
+    def verify_part(part_id, got_bytes, hasher):
+        if got_bytes is not None and got_bytes != parts[part_id]["size"]:
+            msg = "Unexpected part data size in {} part {} (expected {}, got {})"
+            msg = msg.format(dxfile.get_id(), part_id, got_bytes, parts[part_id]["size"])
+            raise DXPartLengthMismatchError(msg)
+        if hasher is not None and hasher.hexdigest() != parts[part_id]["md5"]:
+            msg = "Checksum mismatch in {} part {} (expected {}, got {})"
+            msg = msg.format(dxfile.get_id(), part_id, hasher.hexdigest(), parts[part_id]["md5"])
+            raise DXChecksumMismatchError(msg)
+
+    try:
+        cur_part, got_bytes, hasher = None, None, None
+        # Timeout is required for non-blocking join that can be interrupted by SIGINT (Ctrl+C)
+        for chunk in _get_executor().map(get_chunk, chunks_to_get, timeout=sys.maxint):
+            if chunk["part"] != cur_part:
+                verify_part(cur_part, got_bytes, hasher)
+                cur_part, got_bytes, hasher = chunk["part"], 0, hashlib.md5()
+            got_bytes += len(chunk["data"])
+            hasher.update(chunk["data"])
+            fh.write(chunk["data"])
             if show_progress:
-                _bytes += len(file_content)
+                _bytes += len(chunk["data"])
                 print_progress(_bytes, file_size)
+        verify_part(cur_part, got_bytes, hasher)
+        if show_progress:
+            print_progress(_bytes, file_size, action="Completed")
+    except DXFileError:
+        part_gid = dxfile.get_id() + str(cur_part)
+        print(traceback.format_exc(), file=sys.stderr)
+        _download_retry_counter[part_gid] -= 1
+        if _download_retry_counter[part_gid] > 0:
+            print("Retrying {} ({} tries remain)".format(dxfile.get_id(), _download_retry_counter[part_gid]))
+            return download_dxfile(dxfile_or_id, filename, chunksize=chunksize, append=append,
+                                   show_progress=show_progress, project=project, **kwargs)
+        raise
+    except KeyboardInterrupt:
+        # Call os._exit() in case of KeyboardInterrupt. Otherwise, the atexit registered handler in
+        # concurrent.futures.thread will run, and issue blocking join() on all worker threads, requiring us to
+        # listen to events in worker threads in order to enable timely exit in response to Ctrl-C.
+        print("", file=sys.stderr)
+        os._exit(os.EX_IOERR)
 
-            if len(file_content) == 0:
-                if show_progress:
-                    sys.stderr.write("\n")
-                break
+    if show_progress:
+        sys.stderr.write("\n")
 
-            fd.write(file_content)
+    fh.close()
+
 
 def _get_buffer_size_for_file(file_size, file_is_mmapd=False):
     """Returns an upload buffer size that is appropriate to use for a file

commit 1af634f6f49122ec9c50316fe6484c61869cbca6

PTFM-18747 "dx download" code path issues first req sequentially
Summary:
This change makes it so that clients downloading a file via
dxpy.download_dxfile have 1 download request hit a cold cache instead
of (up to) 8 requests, by waiting until the first request to come back
before starting the (8-thread) parallel thread pool in earnest. This was
done by factoring out the "get first chunk sequentially" that
DXFile.read already uses.

This fixes a regression that was introduced in September 2015 by
384e8ef4. Previously, "dx download" deferred to DXFile.read, which had
the logic for warming up the cache properly. That commit changed it to
an entirely new implementation, which does not (it uses the
ThreadPoolExecutor directly).

Recently, while investigating this issue, I reverted 24eb0c68 (from
February 2016), but that was a red herring. That commit removed the
warm-up protection from DXFile.read, and should be reverted for the most
part. However, the majority of clients, e.g. "dx download", use
dxpy.download_dxfile, which had diverged from DXFile.read, and been
missing the warm-up protection, since September 2015.

Test Plan: tested by hand and checked API load

Reviewers: nicolasbockg

Subscribers: sking

Differential Revision: https://dnanexus.phacility.com/D1149

src/python/dxpy/bindings/dxfile.py
@@ -770,11 +770,12 @@ class DXFile(DXDataObject):
             yield dxpy._dxhttp_read_range, [url, headers, chunk_start_pos, min(chunk_end_pos, self._file_length - 1),
                                             FILE_REQUEST_TIMEOUT], {}
 
-    def _next_response_content(self):
+    def _next_response_content(self, get_first_chunk_sequentially=False):
         if self._response_iterator is None:
             self._response_iterator = dxpy.utils.response_iterator(
                 self._request_iterator,
-                self._http_threadpool
+                self._http_threadpool,
+                do_first_task_sequentially=get_first_chunk_sequentially
             )
         try:
             return next(self._response_iterator)
@@ -825,10 +826,7 @@ class DXFile(DXDataObject):
         # size for this heuristic so we don't incur the overhead for
         # tiny files (which wouldn't contribute as much to the load
         # anyway).
-        if self._file_length > 128 * 1024 and self._pos == 0 and dxpy.JOB_ID:
-            get_first_chunk_sequentially = True
-        else:
-            get_first_chunk_sequentially = False
+        get_first_chunk_sequentially = (self._file_length > 128 * 1024 and self._pos == 0 and dxpy.JOB_ID)
 
         if self._pos == self._file_length:
             return b""
@@ -853,18 +851,7 @@ class DXFile(DXDataObject):
                     self._request_iterator = self._generate_read_requests(
                         start_pos=self._pos, project=project, **kwargs)
 
-                if get_first_chunk_sequentially:
-                    # Make the first chunk request without using the
-                    # usual thread pool and block until it completes. On
-                    # the second chunk, we'll call
-                    # _next_response_content in the alternative block
-                    # below. This starts the threadpool going for the
-                    # second and all subsequent chunks.
-                    _callable, _args, _kwargs = next(self._request_iterator)
-                    content = _callable(*_args, **_kwargs)
-                    get_first_chunk_sequentially = False
-                else:
-                    content = self._next_response_content()
+                content = self._next_response_content(get_first_chunk_sequentially=get_first_chunk_sequentially)
 
                 if len(content) < remaining_len:
                     buf.write(content)

src/python/dxpy/bindings/dxfile_functions.py
@@ -218,10 +218,12 @@ def _download_dxfile(dxid, filename, part_retry_counter,
             raise DXChecksumMismatchError(msg)
 
     with fh:
+        last_verified_pos = 0
+
         if fh.mode == "rb+":
             # We already downloaded the beginning of the file, verify that the
             # chunk checksums match the metadata.
-            last_verified_part, last_verified_pos, max_verify_chunk_size = None, 0, 1024*1024
+            last_verified_part, max_verify_chunk_size = None, 1024*1024
             try:
                 for part_id in parts_to_get:
                     part_info = parts[part_id]
@@ -255,8 +257,11 @@ def _download_dxfile(dxid, filename, part_retry_counter,
 
         try:
             # Main loop. In parallel: download chunks, verify them, and write them to disk.
+            get_first_chunk_sequentially = (file_size > 128 * 1024 and last_verified_pos == 0 and dxpy.JOB_ID)
             cur_part, got_bytes, hasher = None, None, None
-            for chunk_part, chunk_data in response_iterator(chunk_requests(), dxfile._http_threadpool):
+            for chunk_part, chunk_data in response_iterator(chunk_requests(),
+                                                            dxfile._http_threadpool,
+                                                            do_first_task_sequentially=get_first_chunk_sequentially):
                 if chunk_part != cur_part:
                     verify_part(cur_part, got_bytes, hasher)
                     cur_part, got_bytes, hasher = chunk_part, 0, hashlib.md5()

src/python/dxpy/utils/__init__.py
@@ -84,21 +84,30 @@ def wait_for_all_futures(futures, print_traceback=False):
         os._exit(os.EX_IOERR)
 
 
-def response_iterator(request_iterator, thread_pool, max_active_tasks=None):
+def response_iterator(request_iterator, thread_pool, max_active_tasks=None, do_first_task_sequentially=True):
     """
-    :param request_iterator: An iterator producing inputs for consumption by the worker pool.
+    :param request_iterator:
+        An iterator producing inputs for consumption by the worker pool.
     :type request_iterator: iterator of callable, args, kwargs
     :param thread_pool: thread pool to submit the requests to
     :type thread_pool: concurrent.futures.thread.ThreadPoolExecutor
     :param max_active_tasks:
-        The maximum number of tasks that may be either running or waiting for consumption of their result.
-        If not given, defaults to the number of CPU cores on the machine.
+        The maximum number of tasks that may be either running or
+        waiting for consumption of their result. If not given, defaults
+        to the number of CPU cores on the machine.
     :type max_active_tasks: int
+    :param do_first_task_sequentially:
+        If True, executes (and returns the result of) the first request
+        before submitting any other requests (the subsequent requests
+        are submitted with *max_active_tasks* parallelism).
+    :type do_first_task_sequentially: bool
 
-    Rate-limited asynchronous multithreaded task runner.
-    Consumes tasks from *request_iterator*. Yields their results in order, while allowing up to *max_active_tasks* to run
-    simultaneously. Unlike concurrent.futures.Executor.map, prevents new tasks from starting while there are
-    *max_active_tasks* or more unconsumed results.
+    Rate-limited asynchronous multithreaded task runner. Consumes tasks
+    from *request_iterator*. Yields their results in order, while
+    allowing up to *max_active_tasks* to run simultaneously. Unlike
+    concurrent.futures.Executor.map, prevents new tasks from starting
+    while there are *max_active_tasks* or more unconsumed results.
+
     """
     tasks_in_progress = collections.deque()
     if max_active_tasks is None:
@@ -118,6 +127,10 @@ def response_iterator(request_iterator, thread_pool, max_active_tasks=None):
             print('')
             os._exit(os.EX_IOERR)
         return result
+
+    if do_first_task_sequentially:
+        task_callable, task_args, task_kwargs = next(request_iterator)
+        yield task_callable(*task_args, **task_kwargs)
 
     for _i in range(max_active_tasks):
         try:

src/python/test/test_dxpy_utils.py
@@ -100,6 +100,43 @@ class TestResponseIterator(unittest.TestCase):
         for i, res in enumerate(response_iterator(tasks(), get_futures_threadpool(5), max_active_tasks=6)):
             self.assertEqual(i, res)
 
+    def test_first_chunk_sequentially(self):
+        pending_tasks = set()
+
+        def task(i, overlap=False):
+            pending_tasks.add(i)
+            if i >= 1:
+                if overlap:
+                    self.assertIn(0, pending_tasks, "Expected task 0 to be pending during task %d" % i)
+                else:
+                    self.assertNotIn(0, pending_tasks, "Task 0 was pending during task %d" % i)
+            time.sleep(1)
+            pending_tasks.remove(i)
+
+        def tasks(overlap=False):
+            for i in range(8):
+                yield task, [i], dict(overlap=overlap)
+
+        #
+        # |---0---|
+        # |---1---|
+        # |---2---|
+        # |---3---|
+        #
+        # Assert that 0 overlaps with 1, 2, and 3. This assumes that the start
+        # of task 0 happens instantly from the perspective of the calling
+        # thread-- is that reliable?
+        list(response_iterator(tasks(overlap=True), get_futures_threadpool(8), do_first_task_sequentially=False))
+        #
+        # |---0---|
+        #           |---1---|
+        #           |---2---|
+        #           |---3---|
+        #
+        # Assert that 0 doesn't overlap with 1, 2, or 3
+        list(response_iterator(tasks(overlap=False), get_futures_threadpool(8), do_first_task_sequentially=True))
+
+
 class TestDXUtils(unittest.TestCase):
     def test_dxjsonencoder(self):
         f = DXFile("file-" + "x"*24, project="project-" + "y"*24)